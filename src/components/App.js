f3fb620292657dc96d3d43405753a902edaa9db3
thiago <zheka.xreyx@gmail.com>
2021-09-05 17:35:51 +0300

    Small fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import data from '../data/data.json'
 import './App.css';
 import { Table } from './Table/Table.js';
@@ -7,20 +7,24 @@ import { Context } from './Context/Context.js'
 
 function App() {
     const [ search, setSearch ] = useState('');
+    const [ filteredData, setFilteredData] = useState(data);
 
     function filterData(data, search) {
         return data.filter(data => data.name.toLowerCase().includes(search.toLowerCase()));
     }
-
-    const filteredData = filterData(data, search);
+    useEffect(() => {
+        setFilteredData(filterData(data, search));
+    }, [search]);
 
     return (
-        <Context.Provider value={[ search, setSearch ]} >
-            <Search />
+        <>
+            <Context.Provider value={[ search, setSearch ]} >
+                <Search />
+            </Context.Provider >
             <Table 
                 data={filteredData} 
             />
-        </Context.Provider >
+        </>
     );
     
 }
