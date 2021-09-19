006ba5a5d5451d3eff7199d052ca0a9a32480f69
thiago <zheka.xreyx@gmail.com>
2021-09-19 23:48:33 +0300

    Add ESLint and fix some occurred problems

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import data from '../data/data.json'
+import data from '../data/data.json';
 import './App.css';
-import { Table } from './Table/Table.js';
-import { Search } from './Search/Search.js';
-import { Context } from './Context/Context.js'
+import { Table } from './Table/Table';
+import { Search } from './Search/Search';
+import { Context } from './Context/Context';
+
+function filterData(inputData, searchTerm) {
+  return inputData.filter((item) => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+}
 
 function App() {
-    const [ search, setSearch ] = useState('');
-    const [ filteredData, setFilteredData] = useState(data);
+  const [search, setSearch] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
 
-    function filterData(data, search) {
-        return data.filter(data => data.name.toLowerCase().includes(search.toLowerCase()));
-    }
-    useEffect(() => {
-        setFilteredData(filterData(data, search));
-    }, [search]);
+  useEffect(() => {
+    setFilteredData(filterData(data, search));
+  }, [search]);
 
-    return (
-        <>
-            <Context.Provider value={[ search, setSearch ]} >
-                <Search />
-            </Context.Provider >
-            <Table 
-                data={filteredData} 
-            />
-        </>
-    );
-    
+  return (
+    <>
+      <Context.Provider value={[search, setSearch]}>
+        <Search />
+      </Context.Provider>
+      <Table
+        data={filteredData}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,21 +1,21 @@
-import React, { useContext } from "react";
-import { Context } from '../Context/Context.js'
+import React, { useContext } from 'react';
+import { Context } from '../Context/Context';
 import './Search.css';
 
 const searchIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/search.svg';
 
 export const Search = () => {
-    const [ search, setSearch ] = useContext(Context);
-    return (
-        <div id="search-container">
-          <img id="search-icon" alt="" src={searchIconUrl} />
-          <input
-            id="search"
-            type="text"
-            value={search}
-            onChange={({target}) => setSearch(target.value)}
-            placeholder="Search item"
-          />
-        </div>
-    );
-}
+  const [search, setSearch] = useContext(Context);
+  return (
+    <div id="search-container">
+      <img id="search-icon" alt="" src={searchIconUrl} />
+      <input
+        id="search"
+        type="text"
+        value={search}
+        onChange={({ target }) => setSearch(target.value)}
+        placeholder="Search item"
+      />
+    </div>
+  );
+};
